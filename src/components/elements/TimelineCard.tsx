09cd6f0d9ff0865c9f801afbbc0fd34112a05d5c
Michael Schmitt <mail@michaelschmitt.io>
2022-04-14 15:01:08 +0200

    fix: makes timelinecard interactive

--- src/components/elements/TimelineCard.tsx
+++ src/components/elements/TimelineCard.tsx
@@ -4,7 +4,6 @@ import * as PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 // import components
-import Button from '../atoms/Button';
 import Heading from '../atoms/Heading';
 import Text from '../atoms/Text';
 
@@ -22,29 +21,28 @@ const TimelineCard: React.FunctionComponent<{ company: string; title: string; te
 
   // render collabsible
   return (
-    <div className={styles.card}>
-      <div className="relative pr-8">
-        <Heading customClasses="!mb-0" isBlack tag="h3">
-          {title}
-        </Heading>
-        <Text customClasses="!mb-0 text-primary" size="small" isBlack>
-          {company}
-        </Text>
-        <div className="absolute right-0 top-[50%] mt-[-22px]">
-          <Button
-            color="naked"
-            customClasses="!leading-none !tracking-none !text-black pt-[10px]"
-            onClick={() => setIsExpanded(!isExpanded)}
-            size="medium"
-            text={isExpanded ? '↑' : '↓'}
-          />
+    <div className={styles.cardContainer}>
+      <button className={styles.card} onClick={() => setIsExpanded(!isExpanded)} role="button">
+        <div className={styles.header}>
+          <Heading customClasses={styles.headerTitle} isBlack tag="h3">
+            {title}
+          </Heading>
+          <Text customClasses={styles.headerText} size="small" isBlack>
+            {company}
+          </Text>
+          <div className={styles.headerIcon}>
+            <span>{isExpanded ? '↑' : '↓'}</span>
+          </div>
         </div>
-      </div>
-      <div
-        className={classNames('mt-3', { 'flex animate-fade-in': isExpanded, hidden: !isExpanded })}
-      >
-        <Text isBlack>{text}</Text>
-      </div>
+        <div
+          className={classNames(styles.content, {
+            [styles.contentFadeIn]: isExpanded,
+            [styles.contentHidden]: !isExpanded,
+          })}
+        >
+          <Text isBlack>{text}</Text>
+        </div>
+      </button>
     </div>
   );
 };
